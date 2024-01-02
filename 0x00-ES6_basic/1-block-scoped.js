a2fdcbab951cb1eedaea28b753dd604d44fc0ad5
hajarLamharchi <hajar.lamharchi@gmail.com>
2024-01-02 20:41:06 +0100

    fix task1

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,8 +3,9 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
-    let task = true;
-    let task2 = false;
+    /* eslint-disable no-unused-vars */
+    const task = true;
+    const task2 = false;
   }
 
   return [task, task2];
