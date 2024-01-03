6a3a460feab7d9d006bd52c33c36d6c4a8d8946b
hajarLamharchi <hajar.lamharchi@gmail.com>
2024-01-03 19:19:14 +0100

    task2

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,12 @@
+export default function handleResponseFromAPI(promise) {
+  const p = new Promise((resolve, reject) => {
+    if (promise) {
+      resolve({ status: 200, body: 'Success' });
+    } else {
+      reject(new Error(''));
+    }
+  });
+  p.then(() => {
+    console.log('Got a response from the API');
+  });
+}
