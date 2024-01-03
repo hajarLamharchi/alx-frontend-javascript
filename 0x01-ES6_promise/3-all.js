0393acee294753830d375f7eb4caea8c5a2115aa
hajarLamharchi <hajar.lamharchi@gmail.com>
2024-01-03 23:24:13 +0100

    fix

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,11 +1,10 @@
-/* eslint-disable no-console */
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   const promisePhoto = uploadPhoto();
   const promiseUser = createUser();
 
-  Promise.all([promisePhoto, promiseUser]).then(([photo, user]) => {
+  return Promise.all([promisePhoto, promiseUser]).then(([photo, user]) => {
     console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
   }).catch(() => {
     console.log('Signup system offline');
