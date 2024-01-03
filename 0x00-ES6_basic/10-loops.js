260d35652602cf61acb7290d3a2d46fe9e4b1fe4
hajarLamharchi <hajar.lamharchi@gmail.com>
2024-01-03 14:57:29 +0100

    fix task10

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
   for (const idx of array.keys()) {
-    const value = appendString + array[idx];
     /* eslint-disable no-param-reassign */
-    array[idx] = value;
+    const value = array[idx];
+    array[idx] = appendString + value;
   }
 
   return array;
