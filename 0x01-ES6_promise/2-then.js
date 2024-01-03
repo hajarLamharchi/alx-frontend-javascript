7f62ef83c4d74a01764f0f5da3fe9c57af0e7a84
hajarLamharchi <hajar.lamharchi@gmail.com>
2024-01-03 22:39:59 +0100

    fix

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,6 +1,6 @@
 export default function handleResponseFromAPI(promise) {
   promise
     .then(() => ({ status: 200, body: 'Success' }))
-    .catch(() => { return new Error(); })
+    .catch(() => new Error())
     .finally(() => { console.log('Got a response from the API'); });
 }
