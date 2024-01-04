91b8e479f46a5f44e7a8d8dc579861cc2d3331f3
hajarLamharchi <hajar.lamharchi@gmail.com>
2024-01-04 13:47:05 +0100

    task2

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,11 +1,11 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
     if (typeof name !== 'string') {
-      throw new Error('TypeError: Name must be a string');
+      throw new TypeError('Name must be a string');
     } else if (typeof length !== 'number') {
-      throw new Error('TypeError: length must be a number');
+      throw new TypeError('length must be a number');
     } else if (!Array.isArray(students) || students.every((elm) => typeof elm !== 'string')) {
-      throw new Error('TypeError: students must be an array of strings');
+      throw new TypeError('students must be an array of strings');
     } else {
       this._name = name;
       this._length = length;
