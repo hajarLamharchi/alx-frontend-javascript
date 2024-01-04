bd457c586d48fd6c3e6ba57599285236b1191be7
hajarLamharchi <hajar.lamharchi@gmail.com>
2024-01-04 16:58:52 +0100

    task6

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,28 @@
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floors = floors;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(value) {
+    this._sqft = value;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  set floors(value) {
+    this._floors = value;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors`;
+  }
+}
