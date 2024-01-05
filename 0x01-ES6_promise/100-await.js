1975d8b733e42bb8c746db644e95e024b3be99bd
hajarLamharchi <hajar.lamharchi@gmail.com>
2024-01-05 18:56:52 +0100

    fixing task10

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,10 +1,9 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
-  const photo = await uploadPhoto();
-  const user = await createUser();
-
   try {
+    const photo = await uploadPhoto();
+    const user = await createUser();
     return {
       photo,
       user,
