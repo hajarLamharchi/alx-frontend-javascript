75fb0f565d3069b48b7d5491fafd6bd42c726417
hajarLamharchi <hajar.lamharchi@gmail.com>
2024-01-09 14:39:16 +0100

    task11

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,11 +1,9 @@
 export const weakMap = new WeakMap();
 export function queryAPI(endpoint) {
-  let call = 0;
-  if (weakMap.get(endpoint)) {
-    call = weakMap.get(endpoint);
-  }
-  weakMap.set(endpoint, call + 1);
+  let call = weakMap.has(endpoint) ? weakMap.get(endpoint) : 0;
+  call += 1;
   if (call >= 5) {
     throw new Error('Endpoint load is high');
   }
+  weakMap.set(endpoint, call);
 }
